cec616ebd9fdef9689b4749affa28108b2718f7c
Lepo00 <luca.leporati@outlook.com>
2021-04-14 09:27:53 +0200

    cards of grid home

---
+++ src/components/ui/card/Card.tsx
@@ -0,0 +1,20 @@
+import './Card.scss';
+import {Button} from 'antd';
+
+const Card = (props: { title: string }) => {
+  return (
+    <div className="Card">
+      <img src="./assets/wallpaper.jpg" alt="" />
+      <div className="text">
+        <h1>{props.title}</h1>
+        <Button type="primary" shape="round" size="large">
+          Vai all'articolo
+        </Button>
+        {/*293 lettere*/}
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus nisl nec eros dictum, ac ultrices libero ullamcorper. Aenean sagittis vulputate dignissim. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur ac hendrerit turpis. Praesent.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card;
