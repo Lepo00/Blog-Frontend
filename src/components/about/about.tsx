fba8fd7e06f46bbd4057c7480d6d69e5b008c21b
Lepo00 <luca.leporati@outlook.com>
2021-04-20 13:09:19 +0200

    minor fix about

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -3,14 +3,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import { increment,/* signin, signout,*/ getArticles } from '../../store/actions';
 import { RootState } from '../../store/reducers';
 import { useEffect } from 'react';
-import Article from '../../models/Article';
 
 function About(props:any) {
     const dispatch = useDispatch();
     const counter = useSelector((state: RootState) => state.counterReducers);
-    const logged = useSelector((state: RootState) => state.userReducers);
-    const articles: Article[] = useSelector((state: RootState) => state.articleReducers).articles;
-    const loading = useSelector((state: RootState) => state.articleReducers).loading;
 
     useEffect(() => {dispatch(getArticles())}, [dispatch]);
     
@@ -18,12 +14,7 @@ function About(props:any) {
         <div className="About">
             <h1>About</h1>
             <h4>Counter: {counter}</h4>
-            {logged ? <h4>Logged</h4> : <h4>Not Logged in</h4>}
             <button onClick={() => dispatch(increment(5))}>Increase</button>
-            {loading? <h1>Loading</h1>:<h1>Loaded</h1>}
-            {articles.map((article)=>(
-                <h1>{article.title}</h1>
-            ))}
         </div>
     )
 }
