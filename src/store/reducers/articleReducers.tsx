3a89d0573cc13417e6ca9b935496d3da264039c6
Lepo00 <luca.leporati@outlook.com>
2021-04-20 13:09:50 +0200

    remove loading attr

--- src/store/reducers/articleReducers.tsx
+++ src/store/reducers/articleReducers.tsx
@@ -1,8 +1,7 @@
 import { Action, Article } from "../../models";
 
-const initialState: { articles: Article[], loading: boolean, detail: Article } = {
+const initialState: { articles: Article[], detail: Article } = {
     articles: [],
-    loading: true,
     detail: {}
 }
 
@@ -12,25 +11,21 @@ const articleReducer = (state = initialState, action: Action) => {
             return {
                 ...state,
                 articles: action.payload,
-                loading: false
             }
         case "FIRST_ARTICLES":
             return {
                 ...state,
                 articles: action.payload,
-                loading: false
             }
         case "DETAIL_ARTICLE":
             return {
                 ...state,
                 detail: action.payload,
-                loading: false
             }
         case "CREATE_ARTICLE":
             return {
                 ...state,
                 detail: action.payload,
-                loading: false
             }
         default: return state
     }
