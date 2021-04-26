3ce5847da597df76fabccbfa45f490299884f80c
Lepo00 <luca.leporati@outlook.com>
2021-04-26 11:34:50 +0200

    ckeditor installed

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,6 +1,8 @@
 import './About.scss';
 import { Button, message } from 'antd';
 import { RiseOutlined } from '@ant-design/icons';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 function About(props: any) {
 
@@ -16,6 +18,15 @@ function About(props: any) {
         <div className="About">
             <h1>Chi sono</h1>
             <Button size="large" onClick={wip}>Cliccami</Button>
+            <h1>WIP!</h1>
+            <CKEditor
+                    editor={ ClassicEditor }
+                    data=""
+                    onChange={ ( event:any, editor:any ) => {
+                        const data = editor.getData();
+                        console.log( { event, editor, data } );
+                    } }
+                />
         </div>
     )
 }
