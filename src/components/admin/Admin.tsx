d9150574f4f54f6f2004915f7270272b2caea80f
Lepo00 <luca.leporati@outlook.com>
2021-04-27 14:59:45 +0200

    admin page

--- src/components/admin/Admin.tsx
+++ src/components/admin/Admin.tsx
@@ -1,14 +1,22 @@
-import { ItemAdmin } from '../ui';
 import './Admin.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import { ItemAdmin } from '../ui';
+import { RootState } from '../../store/reducers';
+import { useEffect } from 'react';
+import { pendingArticles } from '../../store/actions';
 
 const Admin = () => {
+  const dispatch = useDispatch();
+  const { articles } = useSelector((state: RootState) => state.articleReducers);
+  useEffect(() => { dispatch(pendingArticles()) }, [dispatch]);
   return (
     <div className="Admin">
-      <ItemAdmin/>
-      <ItemAdmin/>
-      <ItemAdmin/>
-      <ItemAdmin/>
-      <ItemAdmin/>
+      <h1>Articoli in attesa di approvazione</h1>
+      {articles.map((article) => (
+        <div>
+          <ItemAdmin {...article} key={article.id}/>
+        </div>
+      ))}
     </div>
   )
 }
--- src/components/ui/list/admin/ItemAdmin.tsx
+++ src/components/ui/list/admin/ItemAdmin.tsx
@@ -1,9 +1,33 @@
 import './ItemAdmin.scss';
+import { Article } from '../../../../models';
+import { Row, Col, Typography, Tooltip, Button, Divider } from 'antd';
+import { CheckOutlined, CloseOutlined } from '@ant-design/icons';
+const { Paragraph } = Typography;
 
-const ItemAdmin = () => {
+const ItemAdmin = (article: Article) => {
   return (
     <div className="ItemAdmin">
-      <h1>STATUS PENDING</h1>
+      <div className="grid-container">
+        <Row wrap={false}>
+          <Col flex="20%" className="img">
+            <img src="http://localhost:8080/blog/image/display/1" alt="" />
+          </Col>
+          <Col flex="auto" className="text">
+            <Paragraph ellipsis={{ rows: 4, expandable: false }}>
+              {article.text}
+            </Paragraph>
+          </Col>
+          <Divider type="vertical" />
+          <Col flex="100px" className="buttons">
+            <Tooltip title="Pubblica articolo">
+              <Button type="primary" shape="circle" size="large" icon={<CheckOutlined />} />
+            </Tooltip>
+            <Tooltip title="Contrassegna articolo come spam" color="red">
+              <Button type="primary" danger shape="circle" size="large" icon={<CloseOutlined />} />
+            </Tooltip>
+          </Col>
+        </Row>
+      </div>
     </div>
   )
 }
