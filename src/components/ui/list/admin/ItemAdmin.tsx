cc6984631c049758bffdcb1239b4fecede7de8fb
Lepo00 <luca.leporati@outlook.com>
2021-04-27 15:49:49 +0200

    fix admin graphic

--- src/components/ui/list/admin/ItemAdmin.tsx
+++ src/components/ui/list/admin/ItemAdmin.tsx
@@ -2,23 +2,24 @@ import './ItemAdmin.scss';
 import { Article } from '../../../../models';
 import { Row, Col, Typography, Tooltip, Button, Divider } from 'antd';
 import { CheckOutlined, CloseOutlined } from '@ant-design/icons';
-const { Paragraph } = Typography;
+const { Paragraph, Title } = Typography;
 
 const ItemAdmin = (article: Article) => {
   return (
     <div className="ItemAdmin">
       <div className="grid-container">
-        <Row wrap={false}>
+        <Row wrap={false} align="middle">
           <Col flex="20%" className="img">
             <img src="http://localhost:8080/blog/image/display/1" alt="" />
           </Col>
           <Col flex="auto" className="text">
-            <Paragraph ellipsis={{ rows: 4, expandable: false }}>
+            <Title level={3} ellipsis={true}>{article.title}</Title>
+            <Paragraph ellipsis={{ rows: 3, expandable: false }}>
               {article.text}
             </Paragraph>
           </Col>
           <Divider type="vertical" />
-          <Col flex="100px" className="buttons">
+          <Col flex="10%" className="buttons">
             <Tooltip title="Pubblica articolo">
               <Button type="primary" shape="circle" size="large" icon={<CheckOutlined />} />
             </Tooltip>
