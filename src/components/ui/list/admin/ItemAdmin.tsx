01cee1abe58ed2b20736f5359b4815e0d5c5972b
Lepo00 <luca.leporati@outlook.com>
2021-04-27 17:14:13 +0200

    remove pending article

--- src/components/ui/list/admin/ItemAdmin.tsx
+++ src/components/ui/list/admin/ItemAdmin.tsx
@@ -2,29 +2,46 @@ import './ItemAdmin.scss';
 import { Article } from '../../../../models';
 import { Row, Col, Typography, Tooltip, Button, Divider } from 'antd';
 import { CheckOutlined, CloseOutlined } from '@ant-design/icons';
+import { useDispatch } from 'react-redux';
+import { deleteArticle, pendingArticles } from '../../../../store/actions';
+import { ButtonShape, ButtonSize, ButtonType } from 'antd/lib/button';
+import { Link } from 'react-router-dom';
 const { Paragraph, Title } = Typography;
 
 const ItemAdmin = (article: Article) => {
+  const style: { type: ButtonType, shape: ButtonShape, size: ButtonSize } = { type: "primary", shape: "circle", size: "large" };
+
+  const dispatch = useDispatch();
+
+  function removeArticle(id: number) {
+    dispatch(deleteArticle(id));
+    dispatch(pendingArticles());
+  }
+
   return (
     <div className="ItemAdmin">
       <div className="grid-container">
         <Row wrap={false} align="middle">
           <Col flex="20%" className="img">
-            <img src="http://localhost:8080/blog/image/display/1" alt="" />
+            <Link to={"/article/" + article.id}>
+              <img src={"http://localhost:8080/blog/image/display/"+article.id} alt="" />
+            </Link>
           </Col>
-          <Col flex="auto" className="text">
-            <Title level={3} ellipsis={true}>{article.title}</Title>
-            <Paragraph ellipsis={{ rows: 3, expandable: false }}>
-              {article.text}
-            </Paragraph>
-          </Col>
+          <Link to={"/article/" + article.id}>
+            <Col flex="auto" className="text">
+              <Title level={3} ellipsis={true}>{article.title}</Title>
+              <Paragraph ellipsis={{ rows: 3, expandable: false }}>
+                {article.text}
+              </Paragraph>
+            </Col>
+          </Link>
           <Divider type="vertical" />
           <Col flex="10%" className="buttons">
             <Tooltip title="Pubblica articolo">
-              <Button type="primary" shape="circle" size="large" icon={<CheckOutlined />} />
+              <Button {...style} icon={<CheckOutlined />} />
             </Tooltip>
             <Tooltip title="Contrassegna articolo come spam" color="red">
-              <Button type="primary" danger shape="circle" size="large" icon={<CloseOutlined />} />
+              <Button danger {...style} icon={<CloseOutlined />} onClick={()=>removeArticle(article.id!)} />
             </Tooltip>
           </Col>
         </Row>
